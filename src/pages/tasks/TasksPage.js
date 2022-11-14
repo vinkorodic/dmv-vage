43046c33a209543fe0f9f5d0b7eb9e994e6b5a1b
Vinko Rodic <rodicvinko4@gmail.com>
2022-11-14 21:24:23 +0100

    going live

--- src/pages/tasks/TasksPage.js
+++ src/pages/tasks/TasksPage.js
@@ -13,7 +13,7 @@ import TableRow from "@mui/material/TableRow";
 
 const TasksWrapper = styled.div`
   width: 100%;
-  max-width: 860px;
+  max-width: 960px;
   margin: auto;
   padding: 20px;
   box-sizing: border-box;
@@ -153,7 +153,7 @@ class TasksPage extends Component {
                 <StyledTableCell align="left">Važi do</StyledTableCell>
                 <StyledTableCell align="left"> Ispostava</StyledTableCell>
                 <StyledTableCell align="left"> Odjeljenje</StyledTableCell>
-                <StyledTableCell align="left"> Napomena</StyledTableCell>
+                <StyledTableCell align="left">Napomena...</StyledTableCell>
               </TableRow>
             </TableHead>
 
